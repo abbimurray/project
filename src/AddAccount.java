0e461a796254fe1de142837a571ef25162184fcd
abbimurray <92475044+abbimurray@users.noreply.github.com>
2024-02-15 11:59:12 +0000

    removed hard coding from addAccount

--- src/AddAccount.java
+++ src/AddAccount.java
@@ -3,6 +3,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class  AddAccount {
     public static void main(String[] args) {
@@ -10,20 +11,33 @@ public class  AddAccount {
         final String DATABASE_URL = "jdbc:mysql://localhost:3306/EVCharging";
         Connection connection = null;
         PreparedStatement pstat = null;
+        Scanner scanner = new Scanner(System.in);
 
-        String firstname = "John";
-        String lastname = "O'Brien";
-        String email ="obrien@gmail.com";
-        String phone ="(086)8578633";
-        String password= "728Vjna?";
-        double currentBalance =60.0;
-        String address = "124 the drive, castlemartin lodge, naas, Co.Kildare";
+        System.out.println("Enter First Name:");
+        String firstname = scanner.nextLine();
+
+        System.out.println("Enter Last Name:");
+        String lastname = scanner.nextLine();
+
+        System.out.println("Enter Email:");
+        String email = scanner.nextLine();
+
+        System.out.println("Enter Phone:");
+        String phone = scanner.nextLine();
+
+        System.out.println("Enter Password:");
+        String password= scanner.nextLine();
+
+        System.out.println("Enter Current Balance:");
+        double currentBalance = scanner.nextDouble();
+
+        System.out.println("Enter Address:");
+        String address = scanner.nextLine();
 
         /*AccountId is primary key + is auto incremented so does it need to also be included as variable or will it just automatically increment for each new account created */
 
 
         int i=0;
-
 
         try {
             //establish connection to database
@@ -50,6 +64,8 @@ public class  AddAccount {
         try {
             pstat.close();
             connection.close();
+            // Close the Scanner
+            scanner.close();
 
         } catch (Exception exception) {
             exception.printStackTrace();
