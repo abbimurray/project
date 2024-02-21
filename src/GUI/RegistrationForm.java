d9445ea3cf77807095c77d77f165de163be087c5
abbimurray <92475044+abbimurray@users.noreply.github.com>
2024-02-21 18:50:39 +0000

    registration

---
+++ src/GUI/RegistrationForm.java
@@ -0,0 +1,19 @@
+package GUI;
+
+import javax.swing.*;
+
+public class RegistrationForm extends JDialog {
+    private JLabel add_icon;
+    private JLabel title;
+    private JLabel userLabel;
+    private JTextField tfUsername;
+    private JLabel emailLabel;
+    private JTextField tfEmail;
+    private JLabel pwLabel;
+    private JPasswordField pwf1;
+    private JLabel pfLabel2;
+    private JPasswordField pwf2;
+    private JButton btnSubmit;
+    private JButton btnCancel;
+    private JPanel registerPanel;
+}
