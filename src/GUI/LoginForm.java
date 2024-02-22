5a1f3c46a15728b169e305b1ceeb6b5b1886b5aa
abbimurray <92475044+abbimurray@users.noreply.github.com>
2024-02-22 16:01:18 +0000

    gui - login

---
+++ src/GUI/LoginForm.java
@@ -0,0 +1,13 @@
+package GUI;
+
+import javax.swing.*;
+
+public class LoginForm {
+    private JLabel iconLogin;
+    private JLabel usernameLabel;
+    private JLabel passwordLabel;
+    private JTextField tfUsername;
+    private JPasswordField pfPassword;
+    private JButton okButton;
+    private JButton btnCancel;
+}
