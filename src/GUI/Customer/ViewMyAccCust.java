09e1e1a715f1fa783382e7fdc038c22b02cbb0a5
abbimurray <92475044+abbimurray@users.noreply.github.com>
2024-02-29 10:45:10 +0000

    gui - checking login and register forms, editing dashboard layouts

---
+++ src/GUI/Customer/ViewMyAccCust.java
@@ -0,0 +1,9 @@
+package GUI.Customer;
+
+import javax.swing.*;
+
+public class ViewMyAccCust {
+    private JPanel headerPanel;
+    private JPanel viewmyAccCustPanel;
+    private JButton btnReturn;
+}
